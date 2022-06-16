f80c70bc5207fd46931ed955a09a34879cdc3d97
Myetekk <99021484+Myetekk@users.noreply.github.com>
2022-06-16 18:02:43 +0200

    Czwartek 16.06
    Próba ogarnięcia systemu losowania

--- Labirynth/Labirynth/Class.h
+++ Labirynth/Labirynth/Class.h
@@ -7,7 +7,7 @@ using namespace std;
 int PlanszaWew[10][10]{ 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 };
 char PlanszaWyswietlana[10][10]{ 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF, 0xCF };
 
-int WielkoscPlanszy = 10; //by³o 6 ale sprawdzam se dla 10
+int WielkoscPlanszy = 10;
 
 class Clas
 {
@@ -124,15 +124,124 @@ void Clas::WyswietlTabliceWew()
 
 void Clas::GenerowanieSciany()
 {
-	PlanszaWew[0][0] = 8;
-	PlanszaWew[WielkoscPlanszy-1][WielkoscPlanszy - 1] = 9;
+	PlanszaWew[0][0] = 8; // oznaczernie poczatku na planszy roboczej
+	PlanszaWew[WielkoscPlanszy-1][WielkoscPlanszy - 1] = 9; // oznaczernie konca na planszy roboczej
 
-	PlanszaWyswietlana[0][0] = ' ';
-	PlanszaWyswietlana[WielkoscPlanszy - 1][WielkoscPlanszy - 1] = ' ';
+	PlanszaWyswietlana[0][0] = ' '; // oznaczernie poczatku na planszy wyswietlanej
+	PlanszaWyswietlana[WielkoscPlanszy - 1][WielkoscPlanszy - 1] = ' '; // oznaczernie konca na planszy wyswietlanej
 
+	//-------------------------------------------------------------------------
+	int kierunek;
+	mt19937 generator(time(nullptr));										   // generator wybierania kierunku
+	uniform_int_distribution<int> kier(1, 4);// 1-gora, 2-prawo, 3-dol, 4-lewo
+	//-------------------------------------------------------------------------
+
+	int aktualnieWiersz= 0, aktualnieKolumna= 0; // zaczynamy w punkcie startowym - w kodzie (0,0), na planszy (1,1)
+	int ostatniRuch{}, przedostatniRuch{};
+
+	int iloscGenerowanychPol{};
+	while (iloscGenerowanychPol <= WielkoscPlanszy * WielkoscPlanszy / 2)
+	{
+		bool dobrze{};
+		while (dobrze == 0)
+		{
+			dobrze = 1;
+			kierunek = kier(generator);
+
+			switch (kierunek)
+			{
+				case 1: //gora
+				{
+					if (aktualnieWiersz > 0 ) // && ostatniRuch == 1 && przedostatniRuch != 1
+					{
+						aktualnieWiersz--;
+						PlanszaWew[aktualnieWiersz][aktualnieKolumna] = 1;
+						PlanszaWyswietlana[aktualnieWiersz][aktualnieKolumna] = ' ';
+						iloscGenerowanychPol++;
+					}
+					else
+						dobrze = 0;
+					break;
+				}
+				case 2: //prawo
+				{
+					if (aktualnieKolumna < WielkoscPlanszy)
+					{
+						aktualnieKolumna++;
+						PlanszaWew[aktualnieWiersz][aktualnieKolumna] = 1;
+						PlanszaWyswietlana[aktualnieWiersz][aktualnieKolumna] = ' ';
+						iloscGenerowanychPol++;
+					}
+					else
+						dobrze = 0;
+					break;
+				}
+				case 3: //dol
+				{
+					if (aktualnieWiersz < WielkoscPlanszy)
+					{
+						aktualnieWiersz++;
+						PlanszaWew[aktualnieWiersz][aktualnieKolumna] = 1;
+						PlanszaWyswietlana[aktualnieWiersz][aktualnieKolumna] = ' ';
+						iloscGenerowanychPol++;
+					}
+					else
+						dobrze = 0;
+					break;
+				}
+				case 4: //lewo
+				{
+					if (aktualnieKolumna > 0)
+					{
+						aktualnieKolumna--;
+						PlanszaWew[aktualnieWiersz][aktualnieKolumna] = 1;
+						PlanszaWyswietlana[aktualnieWiersz][aktualnieKolumna] = ' ';
+						iloscGenerowanychPol++;
+					}
+					else
+						dobrze = 0;
+					break;
+				}
+			}
+			ostatniRuch = kierunek;
+
+		}
+	}
+	
+}
+
+void Clas::GenerowaniePlanszy()
+{//			ilosc generowanych pol
+	/*int x = WielkoscPlanszy* WielkoscPlanszy / 2;
+	for (int i = 1; i <= x; i++)
+	{
+		GenerowanieSciany();
+	}*/
+	GenerowanieSciany();
+
+
+	WyswietlTabliceZew();
+	cout << endl;
+	WyswietlTabliceWew();
+}
+
+
+/*
+
+// generuje miejsce na planszy i jezeli sa spelnione pewne warunki, to usuwa sciane
+void Clas::GenerowanieSciany()
+{
+	PlanszaWew[0][0] = 8; // oznaczernie poczatku planszy na planszy roboczej
+	PlanszaWew[WielkoscPlanszy-1][WielkoscPlanszy - 1] = 9; // oznaczernie konca planszy na planszy roboczej
+
+	PlanszaWyswietlana[0][0] = ' '; // oznaczernie poczatku planszy na planszy wyswietlanej
+	PlanszaWyswietlana[WielkoscPlanszy - 1][WielkoscPlanszy - 1] = ' '; // oznaczernie konca planszy na planszy wyswietlanej
+
+	//-------------------------------------------------------------
 	int kolumna, wiersz;
-	mt19937 generator(time(nullptr));
+	mt19937 generator(time(nullptr));							   // generator wybierania pola
 	uniform_int_distribution<int> pole(0, WielkoscPlanszy - 1);
+	//-------------------------------------------------------------
 
 	bool dobrze{};
 	while (dobrze == 0)
@@ -152,33 +261,13 @@ void Clas::GenerowanieSciany()
 			}
 		}
 	}
-	
-}
 
-void Clas::GenerowaniePlanszy()
-{
-	int x = (2 * WielkoscPlanszy)-3;
-	for (int i = 1; i <= x; i++)
-	{
-		GenerowanieSciany();
-	}
-
-	WyswietlTabliceZew();
-	cout << endl;
-	WyswietlTabliceWew();
 }
 
 
-//
-//if ((licznik == 0 && (PlanszaWew[wiersz - 1][kolumna] == 8 || PlanszaWew[wiersz][kolumna - 1] == 8)) || (licznik != 0 && (PlanszaWew[wiersz - 1][kolumna] == 0 || PlanszaWew[wiersz][kolumna - 1] == 0)))
-//{
-//	if (PlanszaWew[wiersz][kolumna] == 2 || PlanszaWew[wiersz][kolumna] == 8 || PlanszaWew[wiersz][kolumna] == 9)
-//		dobrze = 0;
-//	else
-//	{
-//		dobrze = 1;
-//		PlanszaWew[wiersz][kolumna] = 2;
-//		PlanszaWyswietlana[wiersz][kolumna] = 0xFE; //docelowo do usuniêcia
-//	}
-//}
-////licznik++;
+
+(2 * WielkoscPlanszy)-3
+
+
+
+*/
